b0c6be42628f8d70adfa4ceb3bd74695204fac9a
WaiLimChan1 <wailimchan2@gmail.com>
2024-03-24 15:05:23 -0400

    Azure Blitz Champion added
    Added Azure Blitz Champion with a dash on her Air Attack

---
+++ Assets/_Scripts/Champion Scripts/Specific Champions/Azure Blitz.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AzureBlitz : Champion
+{
+    [Header("Metal Bladekeeper Variables")]
+    [SerializeField] private float dashSpeed = 30;
+
+    public override void ApplyCrowdControl(Champion enemy, float crowdControlStrength)
+    {
+        float direction = 1;
+        if (isFacingLeftNetworked) direction *= -1;
+
+        if (statusNetworked == Status.ATTACK2) enemy.AddVelocity(new Vector2(0, crowdControlStrength));
+        else if (statusNetworked == Status.SPECIAL_ATTACK) enemy.AddVelocity(new Vector2(direction * crowdControlStrength / 2, crowdControlStrength));
+    }
+
+    //---------------------------------------------------------------------------------------------------------------------------------------------
+    protected override void UpdatePosition()
+    {
+        base.UpdatePosition();
+        if (statusNetworked == Status.AIR_ATTACK &&
+            ChampionAnimationController.GetNormalizedTime() >= 2.0f / 8.0f &&
+            ChampionAnimationController.GetNormalizedTime() <= 5.0f / 8.0f)
+        {
+            float xChange = dashSpeed * Time.fixedDeltaTime;
+
+            if (isFacingLeftNetworked) xChange *= -1;
+            Rigid.position = new Vector2(Rigid.position.x + xChange, Rigid.position.y);
+        }
+    }
+    //---------------------------------------------------------------------------------------------------------------------------------------------
+}
