ba63f9d93782821f0220356c049f1fcba171e7d6
SavionW-Works <72318904+SavionW-Works@users.noreply.github.com>
2024-03-31 15:05:16 -0400

    Added Login Menu Functionality
    Players can now
    - Create accounts with emails
    - Login
    - Reset Passwords
    The main menu cannot be accessed until a user logs in

---
+++ Assets/_Scripts/Playfab/PlayfabManager.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using TMPro; 
+
+using UnityEngine.UI;
+
+
+public class PlayfabManager : MonoBehaviour
+{
+
+     
+     public TMP_Text messageText; //Tells users about success/failures 
+     public TMP_InputField emailInput;
+     public TMP_InputField passwordInput;
+
+     //Buttons used in the login menu 
+     public Button LoginClick;
+     public Button RegisterClick;
+     public Button ResetPasswordClick;
+
+     public GameObject loginMenu;
+     public GameObject startMenu;
+
+     void Start()
+     {
+          startMenu.SetActive(false);
+          loginMenu.SetActive(true);
+          LoginClick.onClick.AddListener(() => LoginButton());
+          RegisterClick.onClick.AddListener(() => RegisterButton());
+          ResetPasswordClick.onClick.AddListener(() => ResetPasswordButton());
+     }
+
+     //Makes startMenu active only after login or registration
+     public void nextMenu() {
+          StartCoroutine(ActivateAfterDelay(2));
+          loginMenu.SetActive(false);
+          startMenu.SetActive(true);
+          
+     }
+
+     public void RegisterButton()
+     {
+          if (passwordInput.text.Length < 6)
+          {
+               messageText.text = "Password too short!";
+               return;
+          }
+          var request = new RegisterPlayFabUserRequest
+          {
+               //Parts of a Playfab user request object
+               Email = emailInput.text,
+               Password = passwordInput.text,
+               RequireBothUsernameAndEmail = false
+          };
+
+          PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnError); 
+     } 
+
+     void OnRegisterSuccess(RegisterPlayFabUserResult result)
+     {
+          messageText.text = "Registered!";
+          StartCoroutine(ActivateAfterDelay(1));
+          LoginButton();
+     }
+
+     public void LoginButton()
+     {
+          var request = new LoginWithEmailAddressRequest
+          {
+               Email = emailInput.text,
+               Password = passwordInput.text
+          };
+          PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnError);
+     }  
+
+     void OnLoginSuccess(LoginResult result)
+     {
+          messageText.text = "Logged in!";
+          Debug.Log("Successful login/account create!");
+          nextMenu();
+     }
+
+     public void ResetPasswordButton()
+     {
+          var request = new SendAccountRecoveryEmailRequest
+          {
+               Email = emailInput.text,
+               TitleId = "33451"
+          };
+          PlayFabClientAPI.SendAccountRecoveryEmail(request, OnPasswordReset, OnError);
+     }
+     void OnPasswordReset(SendAccountRecoveryEmailResult result)
+     {
+          messageText.text = "Password reset email sent";
+          Debug.Log("Password reset sent");
+     }
+
+     // Update is called once per frame
+     void Login() 
+     {
+          var request = new LoginWithCustomIDRequest
+          {
+               CustomId = SystemInfo.deviceUniqueIdentifier,
+               CreateAccount = true
+          };
+
+          PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError); 
+     }
+
+     void OnSuccess(LoginResult result)
+     {
+          Debug.Log("Successful login/account create!");
+     }
+
+     void OnError(PlayFabError error)
+     {
+          Debug.Log("Error while logging in/creating account!");
+          Debug.Log(error.GenerateErrorReport());
+     }
+
+     IEnumerator ActivateAfterDelay(float delay)
+     {
+          yield return new WaitForSeconds(delay);
+     }
+}
